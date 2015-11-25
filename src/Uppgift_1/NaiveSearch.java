49e6a77f373f45db2ee257ec9f9f7316fc1f79a8
Emma Shakespeare <emma@wikblad.com>
2015-11-25 10:56:22 +0100

    lade till lite filer

---
+++ src/Uppgift_1/NaiveSearch.java
@@ -0,0 +1,13 @@
+package Uppgift_1;
+
+/**
+ * Användaren uppmanas mata in en sträng att söka efter. Klassen består av en sökfunktion 
+ * som söker efter den inmatade strängen i en textfil.
+ * @author emmashakespeare
+ *
+ */
+public class NaiveSearch {
+	
+	
+
+}
