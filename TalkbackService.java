4a439a453bfabb064a3aac5b4fadf346a62a88ff
Emma Shakespeare <emma@wikblad.com>
2015-11-25 10:18:27 +0100

    Emmatesting

---
+++ TalkbackService.java
@@ -0,0 +1,16 @@
+package Uppgift_1;
+
+import java.io.IOException;
+
+public class TalkbackService implements SerialServer.Service {
+    public String processQuery(String query) {
+	return "<p>You said: '" + query + "'</p>";
+    }
+
+    public static void main(String[] args) throws IOException {
+	TalkbackService service = new TalkbackService();
+        int port = 8888;
+        SerialServer server = new SerialServer(port, service, "talkback");
+        server.run();
+    }
+}
