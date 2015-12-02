3f995958a27438b74ba225d6c33f20db661b82da
Evelyn Gustavsson <Evy@Evelyns-MacBook-Air.local>
2015-12-02 10:32:21 +0100

    Ändrat namn till text och pattern.

--- src/Uppgift_1/NaiveSearch.java
+++ src/Uppgift_1/NaiveSearch.java
@@ -12,58 +12,58 @@ import javax.swing.JOptionPane;
  *
  */
 public class NaiveSearch {
-	private char[] searchIn;
-	private char[] searchFor;
+	private char[] text;
+	private char[] pattern;
 	private int count;
 
 	public NaiveSearch(char[] f) {
-		this.searchIn = f;
+		this.text = f;
 		userInput();
 		searchArray();
 	}
 
 	public void userInput() {
-		String userInput = JOptionPane.showInputDialog(null,
-				"Mata in den sträng som du vill söka efter");
-		this.searchFor = userInput.toCharArray();
+		String userInput = JOptionPane.showInputDialog(null, "Mata in den sträng som du vill söka efter");
+		this.pattern = userInput.toCharArray();
 		this.count = 0;
-
 	}
 
 	/**
-	 * Metoden söker igenom en sträng efter en givet mönster. Om det blir match forstätter jämförelsen i en inre loop.
-	 * Den inre loopen avbryts då mönstret inte matchar och går tillbaka till den yttre loopen som letar vidare i strängen.
+	 * Metoden söker igenom en sträng efter en givet mönster. Om det blir match
+	 * forstätter jämförelsen i en inre loop. Den inre loopen avbryts då
+	 * mönstret inte matchar och går tillbaka till den yttre loopen som letar
+	 * vidare i strängen.
 	 */
 	public void searchArray() {
 		boolean isFound = false;
-		int n = searchIn.length;
-		int m = searchFor.length;
-		System.out.println("Längden på n och m "+ n + " " + m);
+		int n = text.length;
+		int m = pattern.length;
+		System.out.println("Längden på n och m " + n + " " + m);
 
 		int j = 0;
-		for(int i = 0; i < (n - m+1);i++){
+		for (int i = 0; i < (n - m + 1); i++) {
 			System.out.println("i är: " + i);
-			System.out.println("Söker i : " + searchIn[i] );
-			if(searchFor[j] == (searchIn[i])){
+			System.out.println("Söker i : " + text[i]);
+			if (pattern[j] == (text[i])) {
 				System.out.println("Aha! Hittade första bokstaven!");
-				for(j = 1;j < m;j++){
+				for (j = 1; j < m; j++) {
 					System.out.println("Inre loop");
-					if(searchFor[j]==searchIn[i+j]){
-						System.out.println("Match mellan: " + j + " "+ searchFor[j] +  " och " + searchIn[i+j]);
-						if(j == m-1){
-							System.out.println("Klart! Strängen befinner sig på indexposition: "+ i);
+					if (pattern[j] == text[i + j]) {
+						System.out.println("Match mellan: " + j + " " + pattern[j] + " och " + text[i + j]);
+						if (j == m - 1) {
+							System.out.println("Klart! Strängen befinner sig på indexposition: " + i);
 							isFound = true;
 						}
-					}else{
-						System.out.println("Falskt alarm! J är: " + j);
+					} else {
+						System.out.println("Falskt alarm! Vi hoppar ur den inre loopen J är: " + j);
 						break;
 					}
 				}
 			}
 		}
-		if(isFound){
+		if (isFound) {
 			System.out.println("Strängen är funnen!");
-		}else{
+		} else {
 			System.out.println("strängen är ej funnen");
 		}
 
