182198a785277fb2d664cc3a1668a752f53194ec
Emma Shakespeare <emma@wikblad.com>
2015-12-02 10:03:12 +0100

    naive method working, removed jespers classes

--- src/Uppgift_1/NaiveSearch.java
+++ src/Uppgift_1/NaiveSearch.java
@@ -35,6 +35,7 @@ public class NaiveSearch {
 	 * Den inre loopen avbryts då mönstret inte matchar och går tillbaka till den yttre loopen som letar vidare i strängen.
 	 */
 	public void searchArray() {
+		boolean isFound = false;
 		int n = searchIn.length;
 		int m = searchFor.length;
 		System.out.println("Längden på n och m "+ n + " " + m);
@@ -51,15 +52,19 @@ public class NaiveSearch {
 						System.out.println("Match mellan: " + j + " "+ searchFor[j] +  " och " + searchIn[i+j]);
 						if(j == m-1){
 							System.out.println("Klart! Strängen befinner sig på indexposition: "+ i);
+							isFound = true;
 						}
 					}else{
 						System.out.println("Falskt alarm! J är: " + j);
 						break;
 					}
 				}
-				
-				
 			}
+		}
+		if(isFound){
+			System.out.println("Strängen är funnen!");
+		}else{
+			System.out.println("strängen är ej funnen");
 		}
 
 	}
