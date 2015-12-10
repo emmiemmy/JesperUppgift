45a9297cdca0a6a2c6257fa7bab33820062c8ac4
Emma Shakespeare <emma@wikblad.com>
2015-12-10 19:08:47 +0100

    fungerar?

--- src/Uppgift_1/KnuthMorrisPratt.java
+++ src/Uppgift_1/KnuthMorrisPratt.java
@@ -21,32 +21,33 @@ public class KnuthMorrisPratt {
 	}
 
 	public void userInput() {
-		String userInput = JOptionPane.showInputDialog(null, "Mata in den sträng som du vill söka efter");
+		String userInput = JOptionPane.showInputDialog(null,
+				"Mata in den sträng som du vill söka efter");
 		this.pattern = userInput.toCharArray();
 	}
-	
-	public void printArray(char[] array){
-		for(int i = 0; i < array.length;i++){
+
+	public void printArray(char[] array) {
+		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
-		
+
 	}
-	
-	public void printArray(int[] array){
-		for(int i = 0; i < array.length;i++){
+
+	public void printArray(int[] array) {
+		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
-		
+
 	}
 
 	public int[] partialMatchTable(char[] p) {
 		System.out.println("partialMatchTable()");
 		int[] prefixArray = new int[p.length];
-//		prefixArray[1] = 0;
+		// prefixArray[1] = 0;
 		prefixArray[0] = 0;
 		int a = 0;
 		for (int b = 2; b < p.length; b++) {
-//		for (int b = 1; b < p.length; b++) {
+			// for (int b = 1; b < p.length; b++) {
 			while (a > 0 && p[a] != p[b]) {
 				a = prefixArray[a];
 			}
@@ -61,19 +62,19 @@ public class KnuthMorrisPratt {
 	public void search() {
 		System.out.println("search()");
 
-//		int i = 1; // Eventuellt ändra till 0.
-//		int j = 1;
-//		int k = 1;
+		// int i = 1; // Eventuellt ändra till 0.
+		// int j = 1;
+		// int k = 1;
 		int i = 0;
 		int j = 0;
-		int k = 0;
+		int k = 1;
 		int n = text.length;
 		int m = pattern.length;
 
-		while ((n+1 - k) >= m) {
-			System.out.println("Position n-k texten:: " + (n-k) +" Position: k: " + k + " jämför " + text[i] + " med " +  pattern[j]);
+		while ((n + 1 - k) >= m) {
 			while (j < m && text[i] == pattern[j]) {
-				System.out.println("Match! jämför " + text[i] + " med " +  pattern[j]);
+				System.out.println("Match! jämför " + text[i] + " med "
+						+ pattern[j]);
 				i++;
 				j++;
 			}
@@ -81,15 +82,14 @@ public class KnuthMorrisPratt {
 				System.out.println("Positionen för strängen är: " + (k-1));
 			}
 			if (prefix[j - 1] > 0) {
-				k = i - prefix[j-1];
-			} else {
-				if (i == k) {
-					i++;
-				}
-				k = i;
+				k = i - prefix[j - 1];
+			} else if (i == k) {
+				i++;
 			}
+			k = i;
+
 			if (j > 1) {
-				j = prefix[j-1]+1;
+				j = prefix[j - 1] + 1;
 			}
 		}
 	}
