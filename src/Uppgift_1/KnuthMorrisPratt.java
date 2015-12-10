b58bcbdf85b33903b52dfa68cc3a4554a5bef9aa
Evelyn Gustavsson <Evy@Evelyns-MacBook-Air.local>
2015-12-10 19:27:16 +0100

    Funkar

--- src/Uppgift_1/KnuthMorrisPratt.java
+++ src/Uppgift_1/KnuthMorrisPratt.java
@@ -6,8 +6,12 @@ public class KnuthMorrisPratt {
 	private int[] prefix;
 	private char[] text;
 	private char[] pattern;
+	private int counterN;
+	private int counterM;
 
 	public KnuthMorrisPratt(char[] t) {
+		counterN = 0;
+		counterM = 0;
 		this.text = t;
 		userInput();
 		System.out.println("Skriver ut text som en charArray:");
@@ -21,8 +25,7 @@ public class KnuthMorrisPratt {
 	}
 
 	public void userInput() {
-		String userInput = JOptionPane.showInputDialog(null,
-				"Mata in den sträng som du vill söka efter");
+		String userInput = JOptionPane.showInputDialog(null, "Mata in den sträng som du vill söka efter");
 		this.pattern = userInput.toCharArray();
 	}
 
@@ -30,63 +33,61 @@ public class KnuthMorrisPratt {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
-
 	}
 
 	public void printArray(int[] array) {
 		for (int i = 0; i < array.length; i++) {
 			System.out.print(array[i] + " ");
 		}
-
 	}
 
 	public int[] partialMatchTable(char[] p) {
 		System.out.println("partialMatchTable()");
 		int[] prefixArray = new int[p.length];
-		// prefixArray[1] = 0;
 		prefixArray[0] = 0;
 		int a = 0;
+
 		for (int b = 2; b < p.length; b++) {
-			// for (int b = 1; b < p.length; b++) {
 			while (a > 0 && p[a] != p[b]) {
 				a = prefixArray[a];
 			}
+			counterN++;
+			System.out.println("counterN är: " + counterN);
 			if (p[a] == p[b]) {
 				a++;
 			}
 			prefixArray[b] = a;
 		}
+		System.out.println("Antal karaktäristiska operationer för N är: " + counterN++);
 		return prefixArray;
 	}
 
 	public void search() {
 		System.out.println("search()");
 
-		// int i = 1; // Eventuellt ändra till 0.
-		// int j = 1;
-		// int k = 1;
 		int i = 0;
 		int j = 0;
-		int k = 1;
+		int k = 0;
 		int n = text.length;
 		int m = pattern.length;
 
-		while ((n + 1 - k) >= m) {
+		while (((n + 1) - k) >= m) {
 			while (j < m && text[i] == pattern[j]) {
-				System.out.println("Match! jämför " + text[i] + " med "
-						+ pattern[j]);
+				System.out.println("Match! jämför " + text[i] + " med " + pattern[j]);
 				i++;
 				j++;
 			}
 			if (j >= m) {
-				System.out.println("Positionen för strängen är: " + (k-1));
+				System.out.println("Positionen för strängen är: " + (k - 1));
 			}
 			if (prefix[j - 1] > 0) {
 				k = i - prefix[j - 1];
-			} else if (i == k) {
+			} else {
+				if (i == k) {
 				i++;
 			}
 			k = i;
+			}
 
 			if (j > 1) {
 				j = prefix[j - 1] + 1;
